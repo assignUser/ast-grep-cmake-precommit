7d17275b054e8006eb1a144a9585ee4c9efba57e
Jacob Wujciak-Jens <jacob@wujciak.de>
2025-07-16 23:35:33 +0200

    run scan with short report style and no color

--- scripts/cmake-linter.sh
+++ scripts/cmake-linter.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+pkg_root="$NODE_PATH/cmake-linter-precommit"
+ast-grep scan --report-style short --color never -c="$pkg_root/sgconfig.yml" "$@"
 
-echo $(pwd)
+exit $?
